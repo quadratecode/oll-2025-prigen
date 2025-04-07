5af634438977ad9050df017d8b718d843fd7b1f4
Benjamin Arfa <benjamin.arfa.pro@gmail.com>
2025-04-07 11:03:38 +0200

    >fix : fix question titles

--- src/app.py
+++ src/app.py
@@ -111,7 +111,7 @@ def render_question(question, item=None):
 
     # Show help text if available
     if "help" in question:
-        st.markdown(f"**{question_text}**")
+        st.markdown(f"**Frage {question_id} : {question_text}**")
         st.caption(question["help"])
     else:
         st.markdown(f"**{question_text}**")
